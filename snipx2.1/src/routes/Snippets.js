3065b196c4ae2ce5594cb669d429371cd960e198
viktorvelizarov <VVVelizarov18@codingburgas.bg>
2024-08-16 11:45:49 +0200

    merge the changes to Snippets.js component that Sanche made

--- snipx2.1/src/routes/Snippets.js
+++ snipx2.1/src/routes/Snippets.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
+import './Snippets.css';
 import { useAuth } from "../AuthProvider";
 
 function Snippets() {
@@ -7,6 +8,8 @@ function Snippets() {
   const [users, setUsers] = useState([]);
   const [editingSnippetId, setEditingSnippetId] = useState(null);
   const [editingSnippet, setEditingSnippet] = useState({
+    type: "",
+    date: "",
     text: "",
     user_id: "",
     green: "",
@@ -17,18 +20,25 @@ function Snippets() {
     sentiment: "",
   });
 
-  
+  // Separate refs for each textarea
+  const textRef = useRef([]);
+  const greenRef = useRef([]);
+  const orangeRef = useRef([]);
+  const redRef = useRef([]);
+  const explanationsRef = useRef([]);
+  const scoreRef = useRef([]);
+  const sentimentRef = useRef([]);
 
   useEffect(() => {
     const fetchSnippets = async () => {
       try {
         const response = await fetch("https://extension-360407.lm.r.appspot.com/api/company_snippets", {
           method: "POST", 
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-      });
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+        });
         const data = await response.json();
         setSnippets(data);
       } catch (error) {
@@ -48,7 +58,14 @@ function Snippets() {
 
     fetchSnippets();
     fetchUsers();
-  }, []);
+  }, [user]);
+
+  useEffect(() => {
+    // Trigger resize whenever editingSnippetId is set
+    if (editingSnippetId !== null) {
+      resizeAllTextAreas();
+    }
+  }, [editingSnippetId]);
 
   const handleDelete = async (id) => {
     try {
@@ -68,7 +85,9 @@ function Snippets() {
   const handleEditClick = (snippet) => {
     setEditingSnippetId(snippet.id);
     setEditingSnippet({
-      text: snippet.text,
+      type: snippet.type || "",
+      date: snippet.date || "",
+      text: snippet.text || "",
       user_id: snippet.user_id || "",
       green: snippet.green || "",
       orange: snippet.orange || "",
@@ -113,71 +132,80 @@ function Snippets() {
     return user ? user.email : "None";
   };
 
+  const resizeAllTextAreas = () => {
+    [textRef, greenRef, orangeRef, redRef, explanationsRef, scoreRef, sentimentRef].forEach(refGroup => {
+      refGroup.current.forEach(textarea => {
+        if (textarea) {
+          textarea.style.height = "auto"; // Reset height
+          textarea.style.height = `${textarea.scrollHeight}px`; // Set height to content
+        }
+      });
+    });
+  };
+
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-semibold text-gray-800 mb-4">Snippets</h1>
+      <h1 className="text-3xl font-bold text-white mb-6 text-center">Snippets</h1>
 
-      <div className="overflow-x-auto">
-        <table className="min-w-full bg-white border border-gray-300 rounded-lg">
+      <div className="table-wrapper overflow-x-auto">
+        <table className="min-w-full bg-gradient-to-r from-ffb300 to-e4277d border border-gray-300 rounded-lg">
           <thead>
-            <tr className="bg-gray-200">
-              <th className="py-3 px-4 text-left font-medium text-gray-700">ID</th>
-              <th className="py-3 px-4 text-left font-medium text-gray-700">Type</th>
-              <th className="py-3 px-4 text-left font-medium text-gray-700">Date</th>
-              <th className="py-3 px-4 text-left font-medium text-gray-700">Snippet Text</th>
-              <th className="py-3 px-4 text-left font-medium text-gray-700">Green</th>
-              <th className="py-3 px-4 text-left font-medium text-gray-700">Orange</th>
-              <th className="py-3 px-4 text-left font-medium text-gray-700">Red</th>
-              <th className="py-3 px-4 text-left font-medium text-gray-700">Explanations</th>
-              <th className="py-3 px-4 text-left font-medium text-gray-700">Score</th>
-              <th className="py-3 px-4 text-left font-medium text-gray-700">Sentiment</th>
-              <th className="py-3 px-4 text-left font-medium text-gray-700">Assigned User</th>
-              <th className="py-3 px-4 text-left font-medium text-gray-700">Actions</th>
+            <tr className="bg-opacity-80 bg-white">
+              <th className="py-3 px-4 text-left font-bold text-gray-800">ID</th>
+              <th className="py-3 px-4 text-left font-bold text-gray-800">Type</th>
+              <th className="py-3 px-4 text-left font-bold text-gray-800">Date</th>
+              <th className="py-3 px-4 text-left font-bold text-gray-800">Snippet Text</th>
+              <th className="py-3 px-4 text-left font-bold text-gray-800">Green</th>
+              <th className="py-3 px-4 text-left font-bold text-gray-800">Orange</th>
+              <th className="py-3 px-4 text-left font-bold text-gray-800">Red</th>
+              <th className="py-3 px-4 text-left font-bold text-gray-800">Explanations</th>
+              <th className="py-3 px-4 text-left font-bold text-gray-800">Score</th>
+              <th className="py-3 px-4 text-left font-bold text-gray-800">Sentiment</th>
+              <th className="py-3 px-4 text-left font-bold text-gray-800">Assigned User</th>
+              <th className="py-3 px-4 text-left font-bold text-gray-800">Actions</th>
             </tr>
           </thead>
           <tbody>
-            {snippets.map((snippet) => (
-              <tr key={snippet.id} className="border-t border-gray-300">
+            {snippets.map((snippet, index) => (
+              <tr key={snippet.id} className="bg-white bg-opacity-60 border-t border-gray-300">
                 <td className="py-2 px-4">{snippet.id}</td>
-
                 <td className="py-2 px-4">
                   {editingSnippetId === snippet.id ? (
-                    
-                    <textarea
+                    <input
+                      type="text"
                       value={editingSnippet.type}
-                      onChange={(e) =>
-                        setEditingSnippet({ ...editingSnippet, type: e.target.value })
-                      }
-                      className="border rounded px-2 py-1 w-full"
+                      onChange={(e) => {
+                        setEditingSnippet({ ...editingSnippet, type: e.target.value });
+                      }}
+                      className="auto-resize border rounded px-2 py-1 w-full"
                     />
                   ) : (
                     snippet.type || ""
                   )}
                 </td>
-
                 <td className="py-2 px-4">
                   {editingSnippetId === snippet.id ? (
-                    
-                    <textarea
+                    <input
+                      type="text"
                       value={editingSnippet.date}
-                      onChange={(e) =>
-                        setEditingSnippet({ ...editingSnippet, date: e.target.value })
-                      }
-                      className="border rounded px-2 py-1 w-full"
+                      onChange={(e) => {
+                        setEditingSnippet({ ...editingSnippet, date: e.target.value });
+                      }}
+                      className="auto-resize border rounded px-2 py-1 w-full"
                     />
                   ) : (
                     snippet.date || ""
                   )}
                 </td>
-
                 <td className="py-2 px-4">
                   {editingSnippetId === snippet.id ? (
                     <textarea
+                      ref={el => (textRef.current[index] = el)} // Store the ref in textRef
                       value={editingSnippet.text}
-                      onChange={(e) =>
-                        setEditingSnippet({ ...editingSnippet, text: e.target.value })
-                      }
-                      className="border rounded px-2 py-1 w-full"
+                      onChange={(e) => {
+                        setEditingSnippet({ ...editingSnippet, text: e.target.value });
+                      }}
+                      className="auto-resize border rounded px-2 py-1 w-full"
                     />
                   ) : (
                     snippet.text || ""
@@ -185,69 +213,69 @@ function Snippets() {
                 </td>
                 <td className="py-2 px-4">
                   {editingSnippetId === snippet.id ? (
-                    <input
-                      type="text"
+                    <textarea
+                      ref={el => (greenRef.current[index] = el)} // Store the ref in greenRef
                       value={editingSnippet.green}
-                      onChange={(e) =>
-                        setEditingSnippet({ ...editingSnippet, green: e.target.value })
-                      }
-                      className="border rounded px-2 py-1 w-full"
+                      onChange={(e) => {
+                        setEditingSnippet({ ...editingSnippet, green: e.target.value });
+                      }}
+                      className="auto-resize border rounded px-2 py-1 w-full"
                     />
                   ) : (
-                    JSON.stringify(snippet.green) || ""  // Ensure it's a string
+                    snippet.green || ""
                   )}
                 </td>
                 <td className="py-2 px-4">
                   {editingSnippetId === snippet.id ? (
-                    <input
-                      type="text"
+                    <textarea
+                      ref={el => (orangeRef.current[index] = el)} // Store the ref in orangeRef
                       value={editingSnippet.orange}
-                      onChange={(e) =>
-                        setEditingSnippet({ ...editingSnippet, orange: e.target.value })
-                      }
-                      className="border rounded px-2 py-1 w-full"
+                      onChange={(e) => {
+                        setEditingSnippet({ ...editingSnippet, orange: e.target.value });
+                      }}
+                      className="auto-resize border rounded px-2 py-1 w-full"
                     />
                   ) : (
-                    JSON.stringify(snippet.orange) || ""  // Ensure it's a string
+                    snippet.orange || ""
                   )}
                 </td>
                 <td className="py-2 px-4">
                   {editingSnippetId === snippet.id ? (
-                    <input
-                      type="text"
+                    <textarea
+                      ref={el => (redRef.current[index] = el)} // Store the ref in redRef
                       value={editingSnippet.red}
-                      onChange={(e) =>
-                        setEditingSnippet({ ...editingSnippet, red: e.target.value })
-                      }
-                      className="border rounded px-2 py-1 w-full"
+                      onChange={(e) => {
+                        setEditingSnippet({ ...editingSnippet, red: e.target.value });
+                      }}
+                      className="auto-resize border rounded px-2 py-1 w-full"
                     />
                   ) : (
-                    JSON.stringify(snippet.red) || ""  // Ensure it's a string
+                    snippet.red || ""
                   )}
                 </td>
                 <td className="py-2 px-4">
                   {editingSnippetId === snippet.id ? (
-                    <input
-                      type="text"
+                    <textarea
+                      ref={el => (explanationsRef.current[index] = el)} // Store the ref in explanationsRef
                       value={editingSnippet.explanations}
-                      onChange={(e) =>
-                        setEditingSnippet({ ...editingSnippet, explanations: e.target.value })
-                      }
-                      className="border rounded px-2 py-1 w-full"
+                      onChange={(e) => {
+                        setEditingSnippet({ ...editingSnippet, explanations: e.target.value });
+                      }}
+                      className="auto-resize border rounded px-2 py-1 w-full"
                     />
                   ) : (
-                    JSON.stringify(snippet.explanations) || ""  // Ensure it's a string
+                    snippet.explanations || ""
                   )}
                 </td>
                 <td className="py-2 px-4">
                   {editingSnippetId === snippet.id ? (
-                    <input
-                      type="text"
+                    <textarea
+                      ref={el => (scoreRef.current[index] = el)} // Store the ref in scoreRef
                       value={editingSnippet.score}
-                      onChange={(e) =>
-                        setEditingSnippet({ ...editingSnippet, score: e.target.value })
-                      }
-                      className="border rounded px-2 py-1 w-full"
+                      onChange={(e) => {
+                        setEditingSnippet({ ...editingSnippet, score: e.target.value });
+                      }}
+                      className="auto-resize border rounded px-2 py-1 w-full"
                     />
                   ) : (
                     snippet.score || ""
@@ -255,50 +283,31 @@ function Snippets() {
                 </td>
                 <td className="py-2 px-4">
                   {editingSnippetId === snippet.id ? (
-                    <input
-                      type="text"
+                    <textarea
+                      ref={el => (sentimentRef.current[index] = el)} // Store the ref in sentimentRef
                       value={editingSnippet.sentiment}
-                      onChange={(e) =>
-                        setEditingSnippet({ ...editingSnippet, sentiment: e.target.value })
-                      }
-                      className="border rounded px-2 py-1 w-full"
+                      onChange={(e) => {
+                        setEditingSnippet({ ...editingSnippet, sentiment: e.target.value });
+                      }}
+                      className="auto-resize border rounded px-2 py-1 w-full"
                     />
                   ) : (
                     snippet.sentiment || ""
                   )}
                 </td>
-                <td className="py-2 px-4">
-                  {editingSnippetId === snippet.id ? (
-                    <select
-                      value={editingSnippet.user_id}
-                      onChange={(e) =>
-                        setEditingSnippet({ ...editingSnippet, user_id: e.target.value })
-                      }
-                      className="border rounded px-2 py-1"
-                    >
-                      <option value="">Assign to User</option>
-                      {users.map((user) => (
-                        <option key={user.id} value={user.id}>
-                          {user.email}
-                        </option>
-                      ))}
-                    </select>
-                  ) : (
-                    getUserEmail(snippet.user_id) // Get user's email by user_id
-                  )}
-                </td>
+                <td className="py-2 px-4">{getUserEmail(snippet.user_id)}</td>
                 <td className="py-2 px-4">
                   {editingSnippetId === snippet.id ? (
                     <>
                       <button
                         onClick={() => handleSave(snippet.id)}
-                        className="text-green-600 hover:text-green-800 font-medium mr-2"
+                        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-2"
                       >
                         Save
                       </button>
                       <button
                         onClick={handleCancel}
-                        className="text-gray-600 hover:text-gray-800 font-medium"
+                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
                       >
                         Cancel
                       </button>
@@ -307,13 +316,13 @@ function Snippets() {
                     <>
                       <button
                         onClick={() => handleEditClick(snippet)}
-                        className="text-blue-600 hover:text-blue-800 font-medium mr-2"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
                       >
                         Edit
                       </button>
                       <button
                         onClick={() => handleDelete(snippet.id)}
-                        className="text-red-600 hover:text-red-800 font-medium"
+                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
                       >
                         Delete
                       </button>
