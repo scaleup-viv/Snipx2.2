af94acfd75828c490c8714ad0e290062f75edce3
viktorvelizarov <VVVelizarov18@codingburgas.bg>
2024-08-16 14:59:01 +0200

    limit some routes on the navbar to only be shown if you have the admin role

--- snipx2.1/src/routes/NavBar.js
+++ snipx2.1/src/routes/NavBar.js
@@ -10,33 +10,38 @@ const NavBar = () => {
         <>
             <nav>
                 <ul>
-                {user ? (
-                        <li>
-                           <Link to="/login" onClick={logout}> LOGOUT </Link>
-                        </li>
+                    {user ? (
+                        <>
+                            <li>
+                                <Link to="/login" onClick={logout}>LOGOUT</Link>
+                            </li>
+                            <li>
+                                <Link to="/home">CREATE SNIPPETS</Link>
+                            </li>
+                            <li>
+                                <Link to="/weekly-report">WEEKLY REPORT</Link>
+                            </li>
+                            <li>
+                                <Link to="/my-snippets">MY SNIPPETS</Link>
+                            </li>
+
+                            {/* Conditionally render based on user role */}
+                            {user.role === 'admin' && (
+                                <>
+                                    <li>
+                                        <Link to="/snippets">ALL SNIPPETS</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/users">USERS</Link>
+                                    </li>
+                                </>
+                            )}
+                        </>
                     ) : (
                         <li>
                             <Link to="/login">LOGIN</Link>
                         </li>
                     )}
-                    <li>
-                        <Link to="/home">CREATE SNIPPETS</Link>
-                    </li>
-
-                    <li>
-                        <Link to="/weekly-report">WEEKLY REPORT</Link>
-                    </li>
-
-                    <li>
-                        <Link to="/my-snippets">MY SNIPPETS</Link>
-                    </li>
-
-                    <li>
-                        <Link to="/snippets">ALL SNIPPETS</Link>
-                    </li>
-                    <li>
-                        <Link to="/users">USERS</Link>
-                    </li>
                 </ul>
             </nav>
             <Outlet /> {/* This is where the child components will be rendered */}
